a9af6b6401865c6c4de116d64e5321232bef2e19
jcchincheong-bb <jc.chincheong@gmail.com>
2025-08-01 22:54:37 +0200

    Using Numerical data types for increased speed

--- SplineInterpol.m
+++ SplineInterpol.m
@@ -1,16 +1,17 @@
-% Input: data points and end derivatives
-x = [-1 0 1 2 3];                       % x-values
-y = [-28 -10 6 26 56];                  % y-values
+%% Input: data points and end derivatives
+x = [-1 0 1 2 3 4 5 6 7];                       % x-values
+y = [-28 -10 6 26 56 38 16 12 11];                  % y-values
 L = -21;                                % derivative at x1
 R = -37;                                % derivative at xn
 natural = 0;                            % flag variable for natural spline
 n = length(x) - 1;                      % Number of spline intervals
 slope = @(k)((y(k+1)-y(k))/(x(k+1)-x(k)));
 
-% Setup the system
+%% Setup the system
 A = zeros(n+1);               % Coefficient matrix (5x5)
 rhs = zeros(n+1,1);           % Right-hand side
 
+% Clapped Spline set up
 if ~natural
     A(1,1) = (x(2)-x(1))/3; A(1,2) = (x(2)-x(1))/6;
     A(n+1,n) = (x(n+1)-x(n))/6; A(n+1,n+1) = (x(n+1)-x(n))/3;
@@ -18,7 +19,7 @@ if ~natural
     rhs(n+1) = R - (slope(n)); 
 end
 
-
+% Natural Spline Set up
 for i = 2:n
     j = i-1; A(i,j) = (x(j+1)-x(j))/6;
     j = i; A(i,j) = (x(j+1)-x(j-1))/3;
@@ -39,24 +40,23 @@ end
 
 
 
-% Spline function
-syms t
-s = [];
+%% Spline function
+s = zeros(n,4);  % Array of polynomials (3rd order)
 for i = 1:n
-    c = slope(i) - ( z(i+1)/6 + z(i)/3 )*( x(i+1)-x(i) );
-    b = z(i)/2;
     a = (1/6)*( (z(i+1)-z(i)) / (x(i+1)-x(i)) );
-    s = vertcat(s,y(i) + c*(t-x(i)) + b*(t-x(i))^2 + a*(t-x(i))^3);
+    b = z(i)/2;
+    c = slope(i) - ( z(i+1)/6 + z(i)/3 )*( x(i+1)-x(i) );
+    d = y(i);
+    ax = a*conv(conv([1 -x(i)],[1 -x(i)]),[1 -x(i)]);
+    bx = b*conv([1 -x(i)],[1 -x(i)]);
+    cx = c*[1 -x(i)];
+    s(i,:) = ax + [0 bx] + [0 0 cx] + [0 0 0 d];
 end 
 
-% Plotting
+%% Plotting
 plot(x,y,'rx')
 hold on
 for i = 1:n
-    fplot(s(i),[x(i),x(i+1)],'b-')
+    x_span = linspace(x(i),x(i+1));
+    plot(x_span,polyval(s(i,:),x_span),'b-')
 end
-
-% Reference
-poly = [1 -1 16 -10];
-x_span = linspace(x(1),x(n+1));
-plot(x_span,polyval(poly,x_span),'r-')

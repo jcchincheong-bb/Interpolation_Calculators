434f9ca47edd3fcbe9112e7cbc3fd2726dbbc28a
jcchincheong-bb <jc.chincheong@gmail.com>
2025-08-01 23:21:45 +0200

    Added polynomial interoplation
    Also generalised slope calculation

--- SplineInterpol.m
+++ SplineInterpol.m
@@ -5,7 +5,7 @@ L = -21;                                % derivative at x1
 R = -37;                                % derivative at xn
 natural = 0;                            % flag variable for natural spline
 n = length(x) - 1;                      % Number of spline intervals
-slope = @(k)((y(k+1)-y(k))/(x(k+1)-x(k)));
+slope = @(k,x,y)((y(k+1)-y(k))/(x(k+1)-x(k)));
 
 %% Setup the system
 A = zeros(n+1);               % Coefficient matrix (5x5)
@@ -15,8 +15,8 @@ rhs = zeros(n+1,1);           % Right-hand side
 if ~natural
     A(1,1) = (x(2)-x(1))/3; A(1,2) = (x(2)-x(1))/6;
     A(n+1,n) = (x(n+1)-x(n))/6; A(n+1,n+1) = (x(n+1)-x(n))/3;
-    rhs(1) = slope(1) - L; 
-    rhs(n+1) = R - (slope(n)); 
+    rhs(1) = slope(1,x,y) - L; 
+    rhs(n+1) = R - (slope(n,x,y)); 
 end
 
 % Natural Spline Set up
@@ -24,7 +24,7 @@ for i = 2:n
     j = i-1; A(i,j) = (x(j+1)-x(j))/6;
     j = i; A(i,j) = (x(j+1)-x(j-1))/3;
     j = i+1; A(i,j) = (x(j)-x(j-1))/6;
-    rhs(i) = (slope(i)) - (slope(i-1));
+    rhs(i) = (slope(i,x,y)) - (slope(i-1,x,y));
 end
 
 
@@ -43,9 +43,9 @@ end
 %% Spline function
 s = zeros(n,4);  % Array of polynomials (3rd order)
 for i = 1:n
-    a = (1/6)*( (z(i+1)-z(i)) / (x(i+1)-x(i)) );
+    a = (1/6)*( slope(i,x,z) );
     b = z(i)/2;
-    c = slope(i) - ( z(i+1)/6 + z(i)/3 )*( x(i+1)-x(i) );
+    c = slope(i,x,y) - ( z(i+1)/6 + z(i)/3 )*( x(i+1)-x(i) );
     d = y(i);
     ax = a*conv(conv([1 -x(i)],[1 -x(i)]),[1 -x(i)]);
     bx = b*conv([1 -x(i)],[1 -x(i)]);
@@ -59,4 +59,39 @@ hold on
 for i = 1:n
     x_span = linspace(x(i),x(i+1));
     plot(x_span,polyval(s(i,:),x_span),'b-')
+end
+
+%% Interpolated Polynomial - Newton
+x_span = linspace(x(1),x(n+1));
+
+% divided difference scheme
+diffs = zeros(n+1,n+1);
+diffs(:,1) = y(:);                             % First value is the y values
+
+for j = 2:n+1
+    for i = 1:n+2-j
+        diffs(i,j) = (diffs(i+1,j-1) - diffs(i,j-1)) / (x(i+j-1) - x(i));
+    end
+end
+
+coeffs = diffs(1,:);
+
+% evaluating the newton polynomial
+newpoly_val = zeros(1,length(x_span));
+newpoly_val = newpoly_val + coeffs(1);                  % First value is zeroth order
+for k = 1:n
+    temp_poly = rec_conv(x(1:k));
+    newpoly_val = newpoly_val + coeffs(k+1).*polyval(temp_poly,x_span);  % Super position
+end
+
+plot(x_span,newpoly_val,'r-')
+
+function out = rec_conv(x)
+    if length(x) == 1
+        out = [1,-x];
+    elseif length(x) == 2
+        out = conv([1,-x(1)],[1,-x(2)]);
+    else
+        out = conv([1,-x(1)],rec_conv(x(2:end)));
+    end
 end
